dcfb4ce7f9ed1c2286b06509a3f59168903b58f5
BENMALEKarim <fk_benmalek@esi.dz>
2024-04-12 04:16:33 +0100

    setup step 2 of the scenario: Inline value override fix verification

--- override-values/step2/verify.sh
+++ override-values/step2/verify.sh
@@ -12,8 +12,13 @@ LOGFILE=/ks/step2-verify.log
 
 {
     set +e
+    # Retrieve the value of the key "message" from the mock-app release
+    message=$(helm get values --all mock-app -n dev-ns | yq e '.message' -)
 
-    if helm get values --all mock-app -n dev-ns | yq '.message' - | grep "I override the message using an inline value"; then exit 1; fi
+    # Check if the message is not "Test is valid"
+    if [ "$message" != "I override the message using an inline value" ]; then
+        exit 1
+    fi
 
 } >> ${LOGFILE} 2>&1
 
