cea383efbbd8a11937698751ebf22360c9a0b7e4
BENMALEKarim <fk_benmalek@esi.dz>
2024-04-16 17:08:45 +0100

    update step4 verify script

--- automatic-roll-deployment/step4/verify.sh
+++ automatic-roll-deployment/step4/verify.sh
@@ -16,7 +16,7 @@ LOGFILE=/ks/step2-verify.log
     # Retrieve the value of the key "message" from the mock-app release
     export PORT=5000
     export SERVICE_IP=$(kubectl get svc -n dev-ns -l app=mock-app -o jsonpath='{.items[0].spec.clusterIP}')
-    message=(curl -s http://${SERVICE_IP}:${PORT} -w "\n")
+    message=$(curl -s http://${SERVICE_IP}:${PORT} -w "\n")
     # Check if the message is not "Test is valid"
     if [ "$message" != "$target_message" ]; then
         exit 1
